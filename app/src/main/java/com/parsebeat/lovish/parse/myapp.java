5e8725dc3941052b99ace40d6383ab81939f6651
Bajaj, Gaurav <gauravb@telenav.com>
2016-04-19 05:50:49 -0700

    Added fabric/crashlytics to track crashes

--- app/src/main/java/com/parsebeat/lovish/parse/myapp.java
+++ app/src/main/java/com/parsebeat/lovish/parse/myapp.java
@@ -2,8 +2,10 @@ package com.parsebeat.lovish.parse;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.parse.Parse;
 import com.parse.ParseObject;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Lovish-THINKPAD on 15-01-2016.
@@ -16,6 +18,8 @@ public class myapp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
         Parse.enableLocalDatastore(this);
         // Add your initialization code here
         ParseObject.registerSubclass(tracks.class);
